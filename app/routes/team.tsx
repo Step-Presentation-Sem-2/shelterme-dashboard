688f85cd40ce49533d1f8f8163a0ff65e1597bdf
joel-raju <joelraju@ymail.com>
2024-04-13 10:55:07 -0400

    chore: refactored teams page

--- app/routes/team.tsx
+++ app/routes/team.tsx
@@ -1,128 +1,104 @@
-import {Avatar} from 'antd';
-import { Col, Row, Card, Divider, Space } from 'antd';
-import { Typography } from "antd";
-
+import { Col, Row, Card, Divider, Space, Typography } from 'antd';
 import { WithNavigation } from '../components/WithNavigation';
+
 const { Title } = Typography;
 
-const {Meta} = Card
+const { Meta } = Card;
+
+interface TeamMember {
+  name: string;
+  imageUri: string;
+}
 
 export default function Team() {
-  return <WithNavigation>
-  <Title level={2} style={{ marginBottom: '20px', textAlign: 'center' }}>
-    <Space align="center">Teams</Space></Title>
-    <Row  gutter={[16, 16]}>   
-      <Col span={6}>
+  const teamMembers: TeamMember[] = [
+    {
+      name: 'Joel',
+      imageUri: 'app/images/placeholder.jpeg',
+    },
+    {
+      name: 'Aman',
+      imageUri: 'app/images/placeholder.jpeg',
+    },
+    {
+      name: 'Hassaan',
+      imageUri: 'app/images/placeholder.jpeg',
+    },
+    {
+      name: 'Mathew',
+      imageUri: 'app/images/mathew.png',
+    },
+    {
+      name: 'Baldeep',
+      imageUri: 'app/images/placeholder.jpeg',
+    },
+    {
+      name: 'Abhishek',
+      imageUri: 'app/images/abhishek.png',
+    },
+    {
+      name: 'Sneha',
+      imageUri: 'app/images/placeholder.jpeg',
+    },
+    {
+      name: 'Rithwik',
+      imageUri: 'app/images/rithwik.png',
+    },
+    {
+      name: 'Jini',
+      imageUri: 'app/images/placeholder.jpeg',
+    },
+    {
+      name: 'Obitha',
+      imageUri: 'app/images/placeholder.jpeg',
+    },
+    {
+      name: 'Rajesh',
+      imageUri: 'app/images/rajesh.png',
+    },
+    {
+      name: 'Sweatha',
+      imageUri: 'app/images/placeholder.jpeg',
+    },
+    {
+      name: 'Arshia',
+      imageUri: 'app/images/placeholder.jpeg',
+    },
+    {
+      name: 'Govind',
+      imageUri: 'app/images/govind.png',
+    },
+  ];
+
+  const renderTeamMember = (member: TeamMember) => {
+    return (
+      <Col span={6} key={member.name}>
         <Card
           hoverable
           style={{ width: 240 }}
-          cover={<img alt="example" src="app/images/placeholder.jpeg" />}>
-          <Meta title="Joel"  />
-          </Card>
+          cover={<img alt={member.name} src={member.imageUri} />}
+        >
+          <Meta title={member.name} />
+        </Card>
       </Col>
-      <Col span={6}>
-      <Card 
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="example" src="app/images/placeholder.jpeg" />}>
-          <Meta title="Aman"  />
-          </Card>
-      </Col>
-      <Col span={6}>
-      <Card 
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="example" src="app/images/placeholder.jpeg" />}>
-          <Meta title="Hassaan"  />
-          </Card>      </Col>
-      <Col span={6}>
-      <Card 
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="example" src="app/images/mathew.png" />}>
-          <Meta title="Mathew"  />
-          </Card>      </Col>
-    </Row>
-    <Divider />
-    <Row  gutter={[16, 16]}>   
-      <Col span={6}>
-      <Card 
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="example" src="app/images/placeholder.jpeg" />}>
-          <Meta title="Baldeep"  />
-          </Card>      </Col>
-      <Col span={6}>
-      <Card 
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="example" src="app/images/abhishek.png" />}>
-          <Meta title="Abhishek"  />
-          </Card>      </Col>
-      <Col span={6}>
-      <Card 
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="example" src="app/images/placeholder.jpeg" />}>
-          <Meta title="Sneha"  />
-          </Card>      </Col>
-      <Col span={6}>
-      <Card 
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="example" src="app/images/rithwik.png" />}>
-          <Meta title="Rithwik"  />
-          </Card>      </Col>
-    </Row>
-    <Divider />
-    <Row  gutter={[16, 16]}>   
-      <Col span={6}>
-      <Card 
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="example" src="app/images/placeholder.jpeg" />}>
-          <Meta title="Jini"  />
-          </Card>      </Col>
-      <Col span={6}>
-      <Card 
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="example" src="app/images/placeholder.jpeg" />}>
-          <Meta title="Obitha"  />
-          </Card>      </Col>
-      <Col span={6}>
-      <Card 
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="example" src="app/images/rajesh.png" />}>
-          <Meta title="Rajesh"  />
-          </Card>      </Col>
-      <Col span={6}>
-      <Card 
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="example" src="app/images/placeholder.jpeg" />}>
-          <Meta title="Sweatha"  />
-          </Card>      </Col> 
-    </Row>
+    );
+  };
 
-    <Divider />
-
-    <Row  gutter={[16, 16]}>   
-      <Col span={6}>
-      <Card 
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="example" src="app/images/placeholder.jpeg" />}>
-          <Meta title="Arshia"  />
-          </Card>      </Col>
-      <Col span={6}>
-      <Card 
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="example" src="app/images/govind.png" />}>
-          <Meta title="Govind"  />
-          </Card>      </Col>
-    </Row>
-  </WithNavigation>;
+  return (
+    <WithNavigation>
+      <Title level={2} style={{ marginBottom: '20px', textAlign: 'center' }}>
+        <Space align='center'>Teams</Space>
+      </Title>
+      <Row gutter={[16, 16]}>
+        {teamMembers.map((member, index) => {
+          return (
+            <>
+              {renderTeamMember(member)}
+              {(index + 1) % 4 === 0 && <Divider />}
+            </>
+          );
+        })}
+      </Row>
+    </WithNavigation>
+  );
 }
